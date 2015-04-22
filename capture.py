024d8806b0eefd5f3049d689782fa5cbd7153d84
Liam Eagen <liam.eagen@gmail.com>
2015-04-22 17:39:02 -0500

    Allow for passing of weights as command line parameter
    Use the flag '-w <json>' to pass in a weight dictionary for the agents to use in
    a game. This can, obviously, be set once for each running the game.

--- capture.py
+++ capture.py
@@ -629,6 +629,11 @@ def readCommand( argv ):
   parser.add_option('-c', '--catchExceptions', action='store_true', default=False,
                     help='Catch exceptions and enforce time limits')
  
+  # Hack by Liam
+  parser.add_option('-w', '--weights', dest='weightString', default='{}',
+                    help='Specify weights')
+
+
   # Hack by Daniel
   parser.add_option('--frameTime', dest='frameTime', type='float',
                     help=default('Controls the speed of graphical display'), default=0.0)
@@ -636,6 +641,10 @@ def readCommand( argv ):
   options, otherjunk = parser.parse_args(argv)
   assert len(otherjunk) == 0, "Unrecognized options: " + str(otherjunk)
   args = dict()
+  
+  if options.weightString:
+    import ast
+    weights = ast.literal_eval(options.weightString)
 
   # Choose a display format
   #if options.pygame:
@@ -676,7 +685,7 @@ def readCommand( argv ):
     blueArgs['numTraining'] = options.numTraining
   nokeyboard = options.textgraphics or options.quiet or options.numTraining > 0
   print '\nRed team %s with %s:' % (options.red, redArgs)
-  redAgents = loadAgents(True, options.red, nokeyboard, redArgs)
+  redAgents = loadAgents(True, options.red, nokeyboard, redArgs, weights)
   print '\nBlue team %s with %s:' % (options.blue, blueArgs)
   blueAgents = loadAgents(False, options.blue, nokeyboard, blueArgs)
   args['agents'] = sum([list(el) for el in zip(redAgents, blueAgents)],[]) # list of agents
@@ -707,7 +716,7 @@ def randomLayout():
   return layout
 
 import traceback
-def loadAgents(isRed, factory, textgraphics, cmdLineArgs):
+def loadAgents(isRed, factory, textgraphics, cmdLineArgs, weights={}):
   "Calls agent factories and returns lists of agents"
   # Looks through all pythonPath Directories for the right module
   import os
@@ -752,7 +761,9 @@ def loadAgents(isRed, factory, textgraphics, cmdLineArgs):
     traceback.print_exc()
     return [None for i in range(3)]
 
+  foundFactory.weights = weights
   foundFactory = foundFactory(isRed=isRed, **args)
+  
   indexAddend = 0
   if not isRed:
     indexAddend = 1
