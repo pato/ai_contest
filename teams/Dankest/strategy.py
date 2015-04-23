b365b35fcaf6474f54082961ef22cb3d4e4f1f02
Pato <plankenau@gmail.com>
2015-04-23 15:25:58 -0500

    added weights

--- teams/Dankest/strategy.py
+++ teams/Dankest/strategy.py
@@ -111,7 +111,10 @@ class Adaptive(Feature):
 
 class Offensive(Feature):
     # TODO Put contest weights here
-    weights = { 'score': 1.0 }
+    #weights = { 'score': 1.0 }
+    #weights = {'agentFoodDistance':-6.015006944353908,'disperse':0.9162611055588896,'dontStop':-5145.807103151626,'feasts':-55.047115581965,'ghostDistance':44.353568516626716,'ghostFoodDistance':2.078121539302075,'pacmanDistance':-3.637956876253101,'score':494.8849755629843}
+    #weights = {'agentFoodDistance':-6.13169281403951,'disperse':0.1240816945605375,'dontStop':-6174.556841352691,'feasts':-111.18597121422329,'ghostDistance':30.351904504468266,'ghostFoodDistance':1.9795279153381418,'pacmanDistance':-3.645269951232489,'score':675.9370914804919}
+    weights = {'agentFoodDistance':-8.756799781566395,'disperse':0.9092189572995799,'dontStop':-4290.845244111005,'feasts':-125.37448448921515,'ghostDistance':50.23547135970318,'ghostFoodDistance':2.2988748771699736,'pacmanDistance':-3.7381303735807796,'score':783.0366308982319}
 
     def getFeatures(self, agent, gameState, action):
         features = util.Counter()
