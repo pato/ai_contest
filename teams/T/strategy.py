6855d502951f0eebfb145cdb8c0e2866e08d504d
Pato <plankenau@gmail.com>
2015-05-01 21:05:26 -0500

    better weights

--- teams/T/strategy.py
+++ teams/T/strategy.py
@@ -180,7 +180,8 @@ class Offensive(Feature):
     #weights = {'agentFoodDistance':-8.756799781566395,'disperse':0.9092189572995799,'dontStop':-4290.845244111005,'feasts':-125.37448448921515,'ghostDistance':50.23547135970318,'ghostFoodDistance':2.2988748771699736,'pacmanDistance':-3.7381303735807796,'score':783.0366308982319}
     #weights = {'agentFoodDistance':-8.904444886696977,'disperse':1.0414696906450795,'dontStop':-12127.277953518435,'feasts':-1034.1865040893524,'ghostDistance':50.97918744737279,'ghostFoodDistance':1.651378442379878,'pacmanDistance':-29.26105612777171,'score':279.96989732367535}
     #weights = {'agentFoodDistance':-8.651098674855401,'disperse':3.8585068188055973,'dontStop':-10408.254388542864,'feasts':-779.536674122668,'foodDownPath':20.046386707133124,'ghostDistance':53.837701018501875,'ghostFoodDistance':2.5451881318320226,'pacmanDistance':-7.876099711872573,'score':217.31636026497412}
-    weights = {'agentFoodDistance':-8.265134949550575,'disperse':1.6108112643685226,'dontStop':-10000,'feasts':-800,'foodDownPath':15.807036187403249,'ghostDistance':53.8,'ghostFoodDistance':2.5,'pacmanDistance':-7.8,'score':217}
+    #weights = {'agentFoodDistance':-8.265134949550575,'disperse':1.6108112643685226,'dontStop':-10000,'feasts':-800,'foodDownPath':15.807036187403249,'ghostDistance':53.8,'ghostFoodDistance':2.5,'pacmanDistance':-7.8,'score':217}
+    weights={'agentFoodDistance':-8.570681327739509,'disperse':1.4267637933890946,'dontStop':-11053.486700364976,'feasts':-727.964435945695,'foodDownPath':20.071149820762713,'ghostDistance':26.33562578368926,'ghostFoodDistance':2.1033181873413653,'pacmanDistance':-4.400035659018168,'score':867.8759251405045}
     
 
     def getFeatures(self, agent, gameState, action):
